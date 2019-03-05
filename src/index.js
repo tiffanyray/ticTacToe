0a6636fee5347d86830672155ddc6f37b718d787
Tiffany Ray <tiffanyraylee@gmail.com>
2019-03-04 18:51:01 -0600

    added some styling

--- src/index.js
+++ src/index.js
@@ -47,8 +47,7 @@ class Board extends React.Component {
 
     render() {
         return (
-            <div>
-                <div className="status">{status}</div>
+            <div className="board-div">
                 <div className="board-row">
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
@@ -136,7 +135,7 @@ class Game extends React.Component {
                 </div>
                 <div className="game-info">
                     <div>{status}</div>
-                    <ol>{moves}</ol>
+                    <ul>{moves}</ul>
                 </div>
             </div>
         )
@@ -144,25 +143,3 @@ class Game extends React.Component {
 }
 
 ReactDOM.render(<Game />, document.getElementById('root'));
-
-
-// const calculateWinner = (squares) => {
-//     const lines = [
-//         [0, 1, 2],
-//         [3, 4, 5],
-//         [6, 7, 8],
-//         [0, 3, 6],
-//         [1, 4, 7],
-//         [2, 5, 8],
-//         [0, 4, 8],
-//         [2, 4, 6]
-//     ];
-    
-//     for( let i = 0; i < lines.length; i++) {
-//         const [a, b, c] = lines[i];
-//         if (squares[a] && squares[a] === squares[b] && squares[a] === squares[a] === squares[c]) {
-//             return squares[a];
-//         }
-//     } 
-//     return null;
-// };
